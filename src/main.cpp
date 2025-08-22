a4f37b562c71bd60bcf718d16b8bd2db5efc30c6
RealAstolfo <dots@estous.net>
2025-08-22 11:58:42 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -151,6 +151,12 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle)
     g_pConfigManager->reload();
 
     g_Callbacks.push_back(HyprlandAPI::registerCallbackDynamic(
+        PHANDLE, "closeWindow",
+        [&](void* self, SCallbackInfo&, std::any data) {
+            g_pHyprRenderer->damageWindow(std::any_cast<PHLWINDOW>(data));
+        }
+    ));
+    g_Callbacks.push_back(HyprlandAPI::registerCallbackDynamic(
         PHANDLE, "destroyWindow",
         [&](void* self, SCallbackInfo&, std::any data) {
             std::lock_guard<std::mutex> lock(g_ShaderMutex);
